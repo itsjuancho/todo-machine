0efd35c12e100b8d93f23e969399749b58713fde
Andrés Pérez <juanandresperezpt@gmail.com>
2022-06-02 23:54:39 -0500

    Añadidos dos nuevos Hooks State y aplicados try/catch

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,25 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function useLocalStorage(itemName, initialValue) {
-    const localStorageItem = localStorage.getItem(itemName);
-    let parsedItem;
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [item, setItem] = useState(initialValue);
 
-    if (!localStorageItem) {
-        localStorage.setItem(itemName, JSON.stringify(initialValue));
-        parsedItem = initialValue;
-    } else {
-        parsedItem = JSON.parse(localStorageItem);
-    }
+    useEffect(() => {
+        setTimeout(() => {
+            try {
+                const localStorageItem = localStorage.getItem(itemName);
+                let parsedItem;
 
-    const [item, setItem] = useState(parsedItem);
+                if (!localStorageItem) {
+                    localStorage.setItem(itemName, JSON.stringify(initialValue));
+                    parsedItem = initialValue;
+                } else {
+                    parsedItem = JSON.parse(localStorageItem);
+                }
+
+                setItem(parsedItem);
+                setLoading(false);
+            } catch (err) {
+                setError(true);
+            }
+        }, 1500);
+    });
 
     const saveItem = (newTodos) => {
-        const stringifiedTodos = JSON.stringify(newTodos);
-        localStorage.setItem(itemName, stringifiedTodos);
-        setItem(newTodos);
+        try {
+            const stringifiedTodos = JSON.stringify(newTodos);
+            localStorage.setItem(itemName, stringifiedTodos);
+            setItem(newTodos);
+        } catch (error) {
+            setError(true);
+        }
     }
 
-    return [item, saveItem];
+    return {item, saveItem, loading, error};
 }
 
 export { useLocalStorage }
