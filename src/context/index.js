0848138afa5d284ee9c4a5a43d65d49e9e4202ef
Andrés Pérez <juanandresperezpt@gmail.com>
2022-06-03 00:21:27 -0500

    Añadido context de lógica y estado de la app

---
+++ src/context/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const TodoContext = React.createContext();
+
+function TodoProvider(props) {
+    const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage('TODOS_V1', []);
+    const [valueSearch, setValueSearch] = useState('');
+
+    const completedTodos = todos.filter(item => !!item.completed).length;
+    const totalTodos = todos.length;
+
+    let searchedTodos = [];
+
+    if (!valueSearch.length >= 1) {
+        searchedTodos = todos;
+    } else {
+        searchedTodos = todos.filter(todo => {
+            const todoText = todo.text.toLowerCase();
+            const searchText = valueSearch.toLowerCase();
+            return todoText.includes(searchText);
+        });
+    }
+
+    const toggleCompleteTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+        const newTodos = [...todos];
+        newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+        saveTodos(newTodos);
+    }
+
+    const deleteTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+        const newTodos = [...todos];
+        newTodos.splice(todoIndex, 1);
+        saveTodos(newTodos);
+    }
+
+    return (
+        <TodoContext.Provider value={{
+            loading,
+            error,
+            completedTodos ,
+            totalTodos,
+            valueSearch,
+            setValueSearch,
+            searchedTodos,
+            toggleCompleteTodo,
+            deleteTodo
+        }}>
+            {props.children}
+        </TodoContext.Provider>
+    );
+}
+
+export { TodoContext, TodoProvider }
